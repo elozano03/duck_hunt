024724ff22fad2ebf1ffbbb10c7cddc3b08bceeb
coudert <jean.baptiste.coudert1@gmail.com>
2019-10-08 12:02:54 +0200

    finish +

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
 
-var sectionElement = document.getElementsByTagName("section")[0];
+var sectionElement = document.createElement("section");
+document.body.appendChild(sectionElement)
 
-var btn = document.getElementsByTagName("button")[0];
+var divMove = document.createElement("div");
+sectionElement.appendChild(divMove)
 
-var divMove = document.getElementsByTagName("div")[0];
+var btn = document.createElement("button");
+document.body.appendChild(btn)
+btn.innerHTML = "Play"
+
+var newDivMove = document.createElement("div");
+
+
+divMove.addEventListener("click", function(event) {
+    divMove.remove() 
+    // sectionElement.appendChild(divMove)   
+    sectionElement.appendChild(newDivMove) 
+    newDivMove.setAttribute("class", "newdiv")
+    newDivMove.addEventListener("click", function(event) {
+        newDivMove.remove()
+        sectionElement.appendChild(divMove)
+    });
+});
+
 
 btn.addEventListener("click", function () {
     
     
     setInterval(frame, 1000);
-
+    
     function frame() {
+        
         var posX = Math.round(Math.random() * 450);
         var posY = Math.round(Math.random() * 450);
         divMove.style.left = posX + "px";
         divMove.style.top = posY + "px";
         divMove.style.transitionDuration = "2s"
+
+        newDivMove.style.left = posX + "px";
+        newDivMove.style.top = posY + "px";
+        newDivMove.style.transitionDuration = "2s"
     }
 })
 
