966fb7c66d64693c5a23ff47ef2a3d66762f4ce7
coudert <jean.baptiste.coudert1@gmail.com>
2019-10-10 15:07:29 +0200

    init 3

--- script.js
+++ script.js
@@ -9,32 +9,32 @@ var btn = document.createElement("button");
 document.body.appendChild(btn)
 btn.innerHTML = "Play"
 
-for (let i = 0; i < 10; i++) {     
-function killduck(event) {
-    this.remove() 
-    // sectionElement.appendChild(divMove)   
-    divMove = document.createElement("div");
-    sectionElement.appendChild(divMove); 
-    divMove.addEventListener("click", killduck);
-}
-
-divMove.addEventListener("click", killduck);
-
-btn.addEventListener("click", function () {
-    
-    
-    setInterval(frame, 1000);
-    
-    function frame() {
-        
-        var posX = Math.round(Math.random() * 450);
-        var posY = Math.round(Math.random() * 450);
-        divMove.style.left = posX + "px";
-        divMove.style.top = posY + "px";
-        divMove.style.transitionDuration = "2s"
+for (let i = 0; i < 10; i++) {
+    function killduck(event) {
+        this.remove()
+        // sectionElement.appendChild(divMove)   
+        divMove = document.createElement("div");
+        sectionElement.appendChild(divMove);
+        divMove.addEventListener("click", killduck);
     }
-})
+
+    divMove.addEventListener("click", killduck);
+
+    btn.addEventListener("click", function () {
+
+
+        setInterval(frame, 1000);
+
+        function frame() {
+
+            var posX = Math.round(Math.random() * 450);
+            var posY = Math.round(Math.random() * 450);
+            divMove.style.left = posX + "px";
+            divMove.style.top = posY + "px";
+            divMove.style.transitionDuration = "2s"
+        }
+    })
 
 }
-    
+
 
